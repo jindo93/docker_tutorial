2b3393a450e52bc134a5be535570eb0974da672e
jindo93 <doyj2012@gmail.com>
2019-04-14 19:21:56 -0400

    add time for efficiency testing purpose

--- examples/selenium_docker_app/app/main.py
+++ examples/selenium_docker_app/app/main.py
@@ -1,12 +1,17 @@
 from scraper.scraper import scrape_jobs, get_driver
+from time import time
 
 if __name__ == '__main__':
+    start = time()
     driver = get_driver()
-    # , 'software engineer',
     job_titles = ['data scientist', 'software engineer',
                   'data engineer', 'python developer', 'javascript developer']
-    # 'data engineer', 'software developer', 'front-end developer']
     for job in job_titles:
+        local_start = time()
         print(scrape_jobs(driver, job))
+        local_end = time()
+        print('It took ', local_end-local_start,
+              ' to get data for: "{0}"'.format(job))
     driver.quit()
-    # print(scrape_jobs(get_driver()))
+    end = time()
+    print("\n It took total of ", end-start, " time to get all data \n")
